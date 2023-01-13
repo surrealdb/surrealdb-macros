e418d025c959779f3f08b26aabcb8d354492cbb7
rushmorem <rushmore@webenchanter.com>
2023-01-13 07:25:52 +0200

    Implement CANCEL SQL statement

--- src/macros/sql/statements/cancel.rs
+++ src/macros/sql/statements/cancel.rs
@@ -1,19 +1,31 @@
 use proc_macro2::TokenStream;
+use quote::quote;
 use quote::ToTokens;
+use quote::TokenStreamExt;
 use syn::parse::Parse;
 use syn::parse::ParseStream;
 use syn::parse::Result;
+use syn::Error;
+use syn::Ident;
 
 pub struct Statement;
 
 impl Parse for Statement {
-	fn parse(_input: ParseStream) -> Result<Self> {
-		todo!()
+	fn parse(input: ParseStream) -> Result<Self> {
+		let token: Option<Ident> = input.parse()?;
+		if let Some(token) = token {
+			let expected = "TRANSACTION";
+			if token.to_string().to_ascii_uppercase() != expected {
+				let message = format!("Unknown statement `{token}`, did you mean `{expected}`?");
+				return Err(Error::new_spanned(token, message));
+			}
+		}
+		Ok(Self)
 	}
 }
 
 impl ToTokens for Statement {
-	fn to_tokens(&self, _tokens: &mut TokenStream) {
-		todo!()
+	fn to_tokens(&self, tokens: &mut TokenStream) {
+		tokens.append_all(quote!(CancelStatement));
 	}
 }
