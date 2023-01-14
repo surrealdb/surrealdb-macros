0e476472edf2cd32fefe7ee8e89a0fdcbecc6eb4
rushmorem <rushmore@webenchanter.com>
2023-01-14 19:32:36 +0200

    Update error message for empty SQL code

--- src/macros/sql/query.rs
+++ src/macros/sql/query.rs
@@ -16,7 +16,7 @@ pub(super) struct Query(Vec<Statement>);
 impl Parse for Query {
 	fn parse(input: ParseStream) -> Result<Self> {
 		if input.is_empty() {
-			return Err(Error::new(Span::call_site(), "expected SQL code, found nothing"));
+			return Err(Error::new(Span::call_site(), "expected SQL code"));
 		}
 		let statements = Punctuated::<Statement, Token![;]>::parse_terminated(input)?;
 		Ok(Query(statements.into_iter().collect()))
